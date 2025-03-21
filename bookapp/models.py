651c009194490918ddb35a5535573f5ae203d0a9
krishnaharshan <harshankrishna410@gmail.com>
2025-03-21 21:33:53 +0530

    commited sucessful

--- bookapp/models.py
+++ bookapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import user 
+class  UserProfile(models.Model):
+    email=models.EmailField(unique=True)
+    password=models.CharField(max_length="20")
 
-# Create your models here.
+    def __str__(self):
+        return self.email 
+    
+class user():
+    print()
